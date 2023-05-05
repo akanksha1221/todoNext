df8ea1d88e64c8b9bfc7916b372fbfca537f0cc2
Akanksha Kumari <54508862+akanksha00@users.noreply.github.com>
2023-05-05 22:22:43 +0530

    first

--- pages/edit/[title].js
+++ pages/edit/[title].js
@@ -1,42 +1,56 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
-import { useState } from "react";
-const inter = Inter({ subsets: ["latin"] });
+import React, { useEffect, useState } from 'react'
+import {useRouter} from 'next/router'
 
-export default function Home() {
+const Edit = () => {
+    const router = useRouter()
+    const {title} = router.query
 
-  const [todo, setTodo] = useState({title:"", desc:""})
-  const addTodo = () => {
-    let todos = localStorage.getItem("todos")
-    if(todos){
-      let todosJson = JSON.parse(todos)
-      // return length if todo is already present
-      if(todosJson.filter(value=>{return value.title == todo.title}).length > 0){
-        alert("Todo with this title already exists.")
-      }else{
-        todosJson.push(todo)
-        localStorage.setItem("todos",JSON.stringify(todosJson))
-        alert("Todo has been added.")
-        setTodo({title:"", desc:""});
+    const [todo, setTodo] = useState({title: "", desc:""})
+    
+    const updateTodo = () => {
+        let todos = localStorage.getItem("todos")
+        if(todos){
+          let todosJson = JSON.parse(todos)
+          // return length if todo is already present
+          if(todosJson.filter(value=>{return value.title == title}).length > 0){
+            let index = todosJson.findIndex(value=>{return value.title == title})
+            todosJson[index].title = todo.title
+            todosJson[index].desc = todo.desc
+            localStorage.setItem("todos",JSON.stringify(todosJson))
+            alert("Todo has been updated.")
+          }else{
+            alert("Todo does not exist.")
+          }
+          
+        }else{
+          localStorage.setItem("todos",JSON.stringify([todo]))
+        }
       }
-      
-    }else{
-      localStorage.setItem("todos",JSON.stringify([todo]))
-    }
-  }
-  const onChange = (e) =>{
-    setTodo({...todo, [e.target.name]: e.target.value})
-    console.log(todo)
-  }
+      const onChange = (e) =>{
+        setTodo({...todo, [e.target.name]: e.target.value})
+        console.log(todo)
+      }
+    
+    useEffect(()=>{
+        let todos = localStorage.getItem("todos")
+        if(todos){
+            let todosJson = JSON.parse(todos)
+            let ftodo = todosJson.filter(e => title == e.title)
+            if(ftodo.length>0){
+                setTodo(ftodo[0])
+            }
+        }
+    },[router.isReady])
+
   return (
-    <div className="my-2 text-3xl">
+     <div className="my-2 text-3xl">
      
 
       <section class="text-gray-600 body-font">
         <div class="container px-5 py-24 mx-auto flex flex-wrap items-center">
           <div class="bg-gray-100 rounded-lg p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0">
             <h2 class="text-gray-900 text-lg font-medium title-font mb-5">
-              Add a Todo
+              Update the Todo
             </h2>
             <div class="relative mb-4">
               <label for="title" class="leading-7 text-sm text-gray-600">
@@ -64,8 +78,8 @@ export default function Home() {
                 class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
               />
             </div>
-            <button onClick={addTodo} class="text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none w-fit hover:bg-indigo-600 rounded text-lg">
-              Add Todo
+            <button onClick={updateTodo} class="text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none w-fit hover:bg-indigo-600 rounded text-lg">
+              Update Todo
             </button>
             <p class="text-xs text-gray-500 mt-3">
               Literally you probably haven't heard of them jean shorts.
@@ -74,5 +88,7 @@ export default function Home() {
         </div>
       </section>
     </div>
-  );
+  )
 }
+
+export default Edit
